f79b576ae71ef9ed7bfadb68c50e33c1d1d16c02
Vishnu Peram <vjp5266@psu.edu>
2025-11-20 19:11:02 -0800

    basic video processing functionality

---
+++ video-processing-service/src/index.ts
@@ -0,0 +1,34 @@
+import { on } from "events";
+import express from "express";
+import ffmpeg from "fluent-ffmpeg";
+
+const app = express();
+
+app.post("/process-video", (req, res) => {
+    // get path of the input video file form the request body
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if (!inputFilePath || !outputFilePath) {
+        res.status(400).send("Bad Request: Missing file path.");
+    }
+
+    // ffmpeg command
+    ffmpeg(inputFilePath)
+        .outputOptions('-vf', 'scale=-1:360') // 360p
+        .on('end', function(err: any) {
+            console.log('Processing finished successfully');
+            res.status(200).send('Processing finished successfully');
+        })
+        .on('error', function(err: any) {
+            console.log('An error occurred: ' +err.message);
+            res.status(500).send('An error occurred: ' +err.message);
+        })
+        .save(outputFilePath);
+});
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(
+        `Video processing service listening at http://localhost:${port}`);
+});
